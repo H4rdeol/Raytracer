9bade9db0468534980607fc662dbba8e4ca61699
H4rdeol <harde0l@protonmail.com>
2025-09-27 07:15:05 +0200

    [~] WIP

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,6 +6,7 @@
 */
 
 #include <thread>
+#include <print>
 #include "Application/Application.hpp"
 #include "ConfigUpdater/ConfigUpdater.hpp"
 
@@ -13,22 +14,23 @@ int main()
 {
     Application::Application app("assets/configs/test.cfg");
 
-    // std::jthread observer_thread(
-    //     [](const Application::Application &appl) {
-    //         Observer::ConfigUpdater updater;
-    //
-    //         updater.start("assets/configs/test.cfg");
-    //         while (appl.isRunning()) {
-    //             if (auto config = updater.getConfig())
-    //                 appl.camera->update(config.value());
-    //             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    //         }
-    //         updater.stop();
-    //     },
-    //     std::ref(app)
-    // );
+    std::jthread observer_thread(
+        [](const Application::Application &appl) {
+            Observer::ConfigUpdater updater;
 
+            updater.start("assets/configs/test.cfg");
+            while (appl.isRunning()) {
+                if (auto config = updater.getConfig())
+                    appl.camera->update(config.value());
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            }
+            updater.stop();
+        },
+        std::ref(app)
+    );
+
+	std::println("Press A to start the renderer");
     app.run();
-    // observer_thread.join();
+    observer_thread.join();
     return 0;
 }
